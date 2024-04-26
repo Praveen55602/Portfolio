f2b5b297bc3d6eb868581252775df5b1c26c2abe
Praveen <praveensingh55602@gmail.com>
2024-04-26 12:10:11 +0530

    added intro and about sections

---
+++ components/projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+
+function Projects() {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+    </section>
+  );
+}
+
+export default Projects;
